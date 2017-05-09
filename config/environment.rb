9f6b6600a54adbd350e175a55e6acf6c3c1f64dc
kenNg1 <kng1230@hotmail.com>
2017-05-10 00:15:24 +0800

    commit message

---
+++ config/environment.rb
@@ -0,0 +1,15 @@
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["MAILER_EMAIL"],
+  :password => ENV["MAILER_PASSWORD"],
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
