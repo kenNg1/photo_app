9abb99422af015c53dd8b4748538be33e9bd99d7
kenNg1 <kng1230@hotmail.com>
2017-05-10 12:28:33 +0800

    mod registration form,

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,9 +4,11 @@ class Payment < ApplicationRecord
 
   def self.month_options
     Date::MONTHNAMES.compact.each_with_index.map { |name, i | ["#{i+1}-#{name}", i+1]}
+  end
 
   def self.year_options
     (Date.today.year..(Date.today.year+10)).to_a
+  end
 
   def process_payment
     customer = Stripe::Customer.create(email: email, card: token)
