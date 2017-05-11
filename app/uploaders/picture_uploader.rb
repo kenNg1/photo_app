7db038495db94f33947b18888854ec98dc130b60
kenNg1 <kng1230@hotmail.com>
2017-05-11 12:05:51 +0800

    updated validation ins picture_uploader.rb and reduced form

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,5 +1,8 @@
 class PictureUploader < CarrierWave::Uploader::Base
 
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [300,300]
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
